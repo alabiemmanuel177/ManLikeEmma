12cd3d4b45b365097eb808f882472a7fd60e07e6
Emmanuel Alabi <86297924+alabiemmanuel177@users.noreply.github.com>
2023-06-18 19:34:23 +0100

    Update PageLayout.jsx

--- manlikeemma/src/pages/PageLayout/PageLayout.jsx
+++ manlikeemma/src/pages/PageLayout/PageLayout.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import { NavBar } from '../../components/NavBar/NavBar'
-import Background from '../../Animation/World'
+// import Background from '../../Animation/World'
 
 export const PageLayout = ({ theme, handleThemeChange }) => {
 
     return (
         <>
-            <Background />
+{/*             <Background /> */}
             <NavBar theme={theme} handleThemeChange={handleThemeChange}>
                 <Outlet />
             </NavBar>
