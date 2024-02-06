000672ef4307f2c5123f8b06b61315aab39e5d62
Alabi Emmanuel <alabiemmanuel177@gmail.com>
2024-02-06 17:52:41 +0100

    Fixed Contd

--- manlikeemma/manlikeemma/app/component/Experience.tsx
+++ manlikeemma/manlikeemma/app/component/Experience.tsx
@@ -20,6 +20,98 @@ const Experience = () => {
             <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
               <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-gray-20 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
               <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">
+                Nov 2023 - present
+              </header>
+              <div className="z-10 sm:col-span-6">
+                <h3 className="font-medium leading-snug text-slate-200">
+                  <div>
+                    <Link
+                      href={"https://spaybusiness.com/"}
+                      target="_blak"
+                      rel="noreferrer"
+                      aria-label="Software Engineer at Spay Business"
+                      className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-custom focus-visible:text-teal-custom  group/link text-base"
+                    >
+                      <span className="absolute -inset-x-4 -inset-y-2 5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                      <span>
+                        Software Engineer .{" "}
+                        <span className="inline-block">
+                          Spay Business
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
+                            aria-hidden="true"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                              clipRule="evenodd"
+                            ></path>
+                          </svg>
+                        </span>
+                      </span>
+                    </Link>
+                  </div>
+                  <div>
+                    <div className="text-slate-500" aria-hidden="true">
+                      Engineer
+                    </div>
+                  </div>
+                </h3>
+                <p className="mt-2 text-sm leading-normal">
+                  Software Engineer (Spay Business, Jan 2024 - Present): <br />
+                  Developed the landing page for the Swop mobile fintech app
+                  using NextJS and Tailwind. Implemented a push notification
+                  project for merchants using an in-house service, improving
+                  communication and engagement. Collaborated with
+                  cross-functional teams on various projects, ensuring timely
+                  delivery and high-quality code. <br />
+                  <br />
+                  UI/UX Designer (Spay Business, Jan 2024 - Present): <br />
+                  Redesigned the company website UI, enhancing user engagement.
+                  Improved the user interface for key Spay Business products
+                  (checkout, payment link, invoices, direct debit) through user
+                  research and iterative design processes.
+                </p>
+                <ul
+                  className="mt-2 flex flex-wrap"
+                  aria-label="Technologies used"
+                >
+                  <li className="mr-1.5 mt-2">
+                    <div className="flex items-center rounded-full bg-teal-blue px-3 py-1 text-xs font-medium leading-5 text-teal-custom ">
+                      React
+                    </div>
+                  </li>
+                  <li className="mr-1.5 mt-2">
+                    <div className="flex items-center rounded-full bg-teal-blue px-3 py-1 text-xs font-medium leading-5 text-teal-custom ">
+                      RabbitMQ
+                    </div>
+                  </li>
+                  <li className="mr-1.5 mt-2">
+                    <div className="flex items-center rounded-full bg-teal-blue px-3 py-1 text-xs font-medium leading-5 text-teal-custom ">
+                      Java
+                    </div>
+                  </li>
+                  <li className="mr-1.5 mt-2">
+                    <div className="flex items-center rounded-full bg-teal-blue px-3 py-1 text-xs font-medium leading-5 text-teal-custom ">
+                      Springboot
+                    </div>
+                  </li>
+                  <li className="mr-1.5 mt-2">
+                    <div className="flex items-center rounded-full bg-teal-blue px-3 py-1 text-xs font-medium leading-5 text-teal-custom ">
+                      TypeScript
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </li>
+          <li className="mb-12">
+            <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+              <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-gray-20 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+              <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">
                 Aug - Oct 2023
               </header>
               <div className="z-10 sm:col-span-6">
@@ -61,12 +153,13 @@ const Experience = () => {
                   </div>
                 </h3>
                 <p className="mt-2 text-sm leading-normal">
-                  Vulnerability Report System for InfoSec: Collaborated on
-                  developing a system to track and manage security exceptions
-                  for in-house projects. Implemented a process for recording
-                  security exceptions on the application with proposed
-                  solutions, enhancing the bank&apos;s information security
-                  practices.
+                  • Developed a vulnerability report system for InfoSec,
+                  enhancing information security practices by tracking and
+                  managing security exceptions. <br />• Implemented a push
+                  notification project for merchants using an in-house service,
+                  improving communication and engagement. <br />• Collaborated
+                  effectively with cross-functional teams to ensure project
+                  success
                 </p>
                 <ul
                   className="mt-2 flex flex-wrap"
@@ -139,12 +232,13 @@ const Experience = () => {
                   </div>
                 </h3>
                 <p className="mt-2 text-sm leading-normal">
-                  Worked with React JS and Node Express, strengthening both
-                  front-end and back-end development skills. Gained hands-on
-                  experience in the fintech industry, developing a comprehensive
-                  understanding of its challenges and requirements. Developed
-                  non-technical skills such as communication, teamwork, and
-                  problem-solving.
+                  • Built and maintained web applications using React JS and
+                  Node Express, strengthening front-end and back-end development
+                  skills.
+                  <br /> • Gained hands-on experience in the fintech industry,
+                  developing a deep understanding of its challenges and
+                  requirements. <br />• Honed communication, teamwork, and
+                  problem-solving skills through collaborative projects.
                 </p>
                 {/* <ul className="mt-2 flex flex-wrap" aria-label="Related links">
                   <li className="mr-4">
@@ -284,12 +378,13 @@ const Experience = () => {
                   </div>
                 </h3>
                 <p className="mt-2 text-sm leading-normal">
-                  Contributed to diverse projects, showcasing proficiency in
-                  Python scripting, data extraction automation, UI/UX design,
-                  and frontend development. Improved student registration for
-                  the WASSCE by developing Python scripts using image comparison
-                  and name matching algorithms, enhancing accuracy and
-                  efficiency.
+                  • Contributed to diverse projects, showcasing proficiency in
+                  Python scripting, developed a data extraction tool that
+                  reduced processing time by 20%, UI/UX design, and frontend
+                  development. <br />• Developed Python scripts using image
+                  comparison and name matching algorithms that improved W ASSCE
+                  student registration accuracy by 92% and efficiency by 98%,
+                  saving hundreds hours of manual processing time
                 </p>
                 <ul
                   className="mt-2 flex flex-wrap"
@@ -362,13 +457,13 @@ const Experience = () => {
                   </div>
                 </h3>
                 <p className="mt-2 text-sm leading-normal">
-                  Oversaw computer systems, Wi-Fi networks, and CCTV systems.
-                  Performed troubleshooting, diagnostics, and resolved technical
-                  problems to minimize downtime and enhance productivity.
-                  Managed computer hardware installation, upgrades, and
-                  maintenance, including software update Collaborated with
-                  vendors to procure equipment, negotiate pricing, and ensure
-                  timely delivery.
+                  • Oversaw computer systems, Wi-Fi networks, and CCTV systems.
+                  <br />• Troubleshoot and resolved a network connectivity issue
+                  that impacted 50+ users, restoring productivity within few
+                  minutes.
+                  <br />• Managed the installation and upgrade of 20+ computers,
+                  reducing downtime by 90%. <br />• Negotiated discounts on
+                  equipment purchases, saving the company money
                 </p>
               </div>
             </div>
